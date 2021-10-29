a1c1cf6f7eeb3877b3a0c7a1d7c00909982d9393
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-10-29 19:23:45 -0300

    solving req 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuario_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(usuario_id_param INT)
 RETURNS INT READS SQL DATA
 BEGIN
 
 DECLARE musicas_historico INT;
 SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes
 GROUP BY USUARIO_ID
-HAVING USUARIO_ID = usuario_id INTO musicas_historico;
+HAVING USUARIO_ID = usuario_id_param INTO musicas_historico;
 RETURN musicas_historico;
 
 
